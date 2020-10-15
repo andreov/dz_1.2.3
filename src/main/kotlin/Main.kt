f2d5497cb4f84a0d48dd835a83161c27fc8bef1e
AndreyOvch <amo-rzn@ya.ru>
2020-10-15 18:48:56 +0300

    edit discount meloman

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,13 @@
 fun main(){
-    val currentAmount = 100.0
-    val prevAmount = 10001
+    val currentAmount = 1000.0
+    val prevAmount = 100
     val discountMin = 100.0
     val discountMax = currentAmount*5/100
     val isDiscountSuper = true
-    val discountSuper=(currentAmount-discountMax)/100+discountMax
     val result=if(prevAmount in 1001..10_000){
                           println("Скидка: $discountMin руб.")
                           currentAmount-discountMin
-                      }else if(isDiscountSuper && prevAmount>=10001){
-                                 println("Скидка для меломана: $discountSuper руб.")
-                                 currentAmount-discountSuper
+
                       }else if(prevAmount>=10_001){
                                println("Скидка: $discountMax")
                                currentAmount-discountMax
@@ -19,7 +16,10 @@ fun main(){
                               currentAmount
                       }
 
-    println("Общая сумма: $result руб.")
+    val discountSuper=result/100
+    val resultSuper = result-discountSuper
+    if(isDiscountSuper) println("Скидка для меломана $discountSuper \n" + "Общая сумма покупки для меломана: $resultSuper руб.")
+    else println("Общая сумма покупки: $result руб.")
 
 
 }
